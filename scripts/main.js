5c45ccbcb15c4da878f0201daad3b8442030b7c3
earezza <>
2023-07-24 10:50:03 -0400

    pistuff

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+<!--
+            $(document).ready(function () 
+            {
+                $.get("/images/picam/", function(data) 
+                {
+                    document.getElementById("photos").innerHTML = data;
+                    console.log(data)
+        
+                });
+                
+            })
+        -->
